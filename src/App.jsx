f295b69971cb11ee707be7fe23675ecba87be6eb
Muhammad Moazzam <rajamoazzam789@gmail.com>
2023-05-28 00:24:58 +0500

    cm

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard  from "./MovieCard";
 
-const API_URL = "http://www.omdbapi.com?apikey=83b390b";
+const API_URL = "https://www.omdbapi.com?apikey=83b390b";
 
 // const movie1 = {
 //   "Title": "Amazing Spiderman Syndrome",
